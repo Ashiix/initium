b56e4ec2cfe7a18ffbf0f3fd17cd6d393a9b4b47
Ash <35050991+Ashiix@users.noreply.github.com>
2020-12-27 19:05:57 -0500

    Nearly autonomous time-based reroll bot
    Adding auto-run to escape after hitting troll

--- reroll.js
+++ reroll.js
@@ -1,30 +1,39 @@
 // Initium rerolling bot
 
-// The bot will automatically hit a troll 11 times, the stats you want for a high roll alt after 11 hits are: 5.07/5.03/5.01 
-
 // Enter your charecter name of choice below
 var characterName = 'Kathryne';
+
 
 // Stolen from https://www.sitepoint.com/delay-sleep-pause-wait/
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function main() {
-  deleteAndRecreateCharacter(event, characterName);
-  $(document.getElementsByClassName('popup_confirm_option confirm_yes')).click();
-  $(document.getElementsByClassName('popup_confirm_option confirm_okay')).click();
-  await sleep(5000);
-  doGoto(event, 5707702298738688);
-  await sleep(10000);
-  createCampsite();
-  await sleep(500);
-  $(document.getElementsByClassName('popup_confirm_option confirm_okay')).click();
-  await sleep(5000);
-  for (i = 0; i < 11; i++) {
-    doCombatAttackLeftHand(event);
-    await sleep(1500);
+async function reroll() {
+  while(true) {
+    deleteAndRecreateCharacter(event, characterName);
+    $(document.getElementsByClassName('popup_confirm_option confirm_yes')).click();
+    $(document.getElementsByClassName('popup_confirm_option confirm_okay')).click();
+    await sleep(5000);
+    doGoto(event, 5707702298738688);
+    await sleep(10000);
+    createCampsite();
+    $(document.getElementsByClassName('popup_confirm_option confirm_okay')).click();
+    await sleep(5000);
+    for (i = 0; i < 11; i++) {
+      doCombatAttackLeftHand(event);
+      await sleep(100);
+      closepopupMessage();
+      await sleep(1500);
+    }
+    var [strength, dexterity, intelligence] = document.getElementById("newui").innerHTML.match("minitip=\"(.*)<br>")[1].split("/");
+    if (strength >= 5.07 && dexterity >= 5.03 && intelligence >= 5.01) {
+      alert("High stat roll alt found");
+      break;
+    } else {
+      console.log("Normal alt found");
+    }
   }
 }
 
-main();
+reroll();
